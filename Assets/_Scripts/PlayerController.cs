57e036db0945a6849bd73e8ec5f208c80c734fba
taerakwon <taerakwon@gmail.com>
2016-10-14 19:29:13 -0400

    Sprites, Sounds, and Font assets were added.  Class for person added

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿/* 
+Game Title: Survival Instrinct
+Created By: Taera Kwon (#300755802)
+Last Edited By: Taera Kwon
+Last Edited Date: Oct 14, 2016
+Short Revision: This class is for player controll
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	// PRIVATE VARIABLES
+	float _playerSpeed;
+
+	// PUBLIC VARIABLES
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// PRIVATE METHOD
+
+
+	// PUBLIC METHOD
+}
