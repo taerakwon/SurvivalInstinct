9a135ce7fc09e8aafb5bc3b77477b4e6c3b38176
taerakwon <taerakwon@gmail.com>
2016-10-25 18:34:46 -0400

    Added Bulletin Message

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,84 @@
+﻿/* 
+Game Title: Survival Instrinct
+Created By: Taera Kwon (#300755802)
+Last Edited By: Taera Kwon
+Last Edited Date: Oct 19, 2016
+Short Revision: This class is for Game Controllers (Ex- Scores, behaviours)
+History: 
+Oct-25: Message (Bulletin Message) Added
+Oct-21:
+		Created
+*/
+
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	// Private Instances
+	private int _livesValue;
+	private int _foodValue;
+	private bool _messageVisible;
+
+	// Public Instances
+
+	public Text LivesLabel;
+	public Text FoodLabel;
+
+	[Header("Message")]
+	public GameObject BulletinMessage;
+
+	// Use this for initialization
+	void Start () {
+		this._livesValue = 4;
+		this._foodValue = 0;
+		this._messageVisible = false;
+		this.BulletinMessage.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.LivesLabel.text = "LIVES LEFT: " + this._livesValue;
+		this.FoodLabel.text = "FOOD COLLECTED: " + this._foodValue;
+		if (this._messageVisible) {
+			this.BulletinMessage.SetActive (true);
+		} else {
+			this.BulletinMessage.SetActive (false);			
+		}
+	}
+
+	// Public Method
+
+	public int LivesValue
+	{
+		get{
+			return this._livesValue;
+		}
+		set{
+			this._livesValue = value;
+		}
+	}
+
+	public int FoodValue
+	{
+		get{
+			return this._foodValue;
+		}
+		set{
+			this._foodValue = value;
+		}
+	}
+
+	// Visibility of Message
+	public bool MessageVisibility
+	{
+		get{
+			return this._messageVisible;
+		}
+		set{
+			this._messageVisible = value;
+		}
+	}
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,10 +2,10 @@
 Game Title: Survival Instrinct
 Created By: Taera Kwon (#300755802)
 Last Edited By: Taera Kwon
-Last Edited Date: Oct 19, 2016
+Last Edited Date: Oct 21, 2016
 Short Revision: This class is for player control
 History: 
-
+Oct-25: Added bulletin board
 Oct-19: Added respawn feature
 		Added DeathPlane Interaction
 		Changed Timescale
@@ -24,10 +24,13 @@ public class PlayerController : MonoBehaviour {
 	private bool _isFacingRight;
 	private bool _isGrounded; // Checks if player is grounded
 	private float _jump;
+	private bool _isBulletinVisible; // If Player is on BulletinBoard
+
 	private Animator _animator;
 	private GameObject _mainCamera;
 	private GameObject _SpawnPoint;
-	private int _lives;
+	private GameObject _gameControllerObject;
+	private GameController _gameController;
 
 	// PUBLIC VARIABLES
 	public float PlayerSpeed = 25f;
@@ -38,6 +41,8 @@ public class PlayerController : MonoBehaviour {
 	[Header("Sounds")]
 	public AudioSource JumpSound;
 	public AudioSource ManScream;
+	public AudioSource PickupSound;
+	public AudioSource OuchSound;
 
 	// Use this for initialization
 	void Start () {
@@ -106,16 +111,18 @@ public class PlayerController : MonoBehaviour {
 	// Initialises character variables
 	private void _initialise()
 	{
-		this._lives = 4;
 		this._jump = 0f;
 		this._transform = GetComponent<Transform> (); // Gets Transform component of player
 		this._rigidbody = GetComponent<Rigidbody2D> (); // Gets Rigidbody2D component of player
 		this._animator = GetComponent<Animator>(); // Gets Animator of component
 		this._mainCamera = GameObject.FindWithTag("MainCamera");
 		this._SpawnPoint = GameObject.FindWithTag("SpawnPoint");
+		this._gameControllerObject = GameObject.Find ("Game Controller");
+		this._gameController = this._gameControllerObject.GetComponent<GameController> ();
 		this._move = 0f;
 		this._isFacingRight = true;
 		this._isGrounded = false;
+		this._isBulletinVisible = false; // Player cannot see Bulletin Board
 
 	}
 
@@ -133,9 +140,37 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("DeathPlane")) {
 			// transport player's position to spawn point
 			this.ManScream.Play ();
-			this._lives -= 1;
+			this._gameController.LivesValue -= 1;
 			this._transform.position = this._SpawnPoint.transform.position; //Respawn
 		}
+
+		// When you pickup item
+		if (other.gameObject.CompareTag ("Item")) {
+			this.PickupSound.Play ();
+			this._gameController.FoodValue += 1;
+			Destroy (other.gameObject);
+		}
+
+		if (other.gameObject.CompareTag ("BulletinBoard")) {
+			this._gameController.MessageVisibility = true;				
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("BulletinBoard")) {
+			this._gameController.MessageVisibility = false;				
+		}
+	}
+
+	// When play is colliding (instance)
+	private void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.CompareTag ("Enemy")) {
+			this._gameController.LivesValue -= 1;
+			this.OuchSound.Play ();
+			this._transform.position = this._SpawnPoint.transform.position; // Respawn
+		}
+	
 	}
 
 	// When player is colliding
